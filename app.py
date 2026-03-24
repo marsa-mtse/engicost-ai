a76650422e3a53fbd338e03382c636ec38531c73
mohamed arab <mohamedarab2@users.noreply.huggingface.co>
2026-03-24 19:15:25 +0000

    Upload app.py with huggingface_hub

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import auth
 import utils.__init__ as utils
+from utils import t
 import database
 from ai_engine.market_api import MarketEngine
 
